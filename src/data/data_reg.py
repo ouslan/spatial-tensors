ed4bfcb4a3cfa23f088fff63f139384068a98246
Alejandro Ouslan <alejandro.ouslan@upr.edu>
2025-10-16 19:34:42 -0400

    added simulation with other weigths

--- src/data/data_reg.py
+++ src/data/data_reg.py
@@ -1,16 +1,14 @@
-import os
-
 import logging
 import arviz as az
-import bambi as bmb
 import geopandas as gpd
 import numpy as np
 import pandas as pd
 import polars as pl
 from pysal.lib import weights
 from shapely import wkt
-from shapely.geometry import Polygon
-from spreg import Panel_FE_Lag, dgp_lag
+from spreg import dgp_lag
+import statsmodels.api as sm
+import pymc as pm
 
 from .data_pull import DataPull
 
@@ -21,15 +19,26 @@ class SpatialReg(DataPull):
         saving_dir: str = "data/",
         database_file: str = "data.ddb",
         log_file: str = "data_process.log",
-        seed: int = 787,
     ):
         super().__init__(saving_dir, database_file, log_file)
-        self.seed = seed
+
+        # Define the spatial weight matrix
+        self.wr = weights.contiguity.Rook.from_dataframe(
+            self.spatial_df(), use_index=False
+        )
+
+        self.wq = weights.contiguity.Queen.from_dataframe(
+            self.spatial_df(), use_index=False
+        )
+        self.wq.transform = "r"
+
+        self.wk6 = weights.KNN.from_dataframe(self.spatial_df(), k=6, use_index=False)
+        self.wk6.transform = "r"
 
     def spatial_data(
-        self, mu: int, sigma: int, rho: float, time: int
+        self, mu: int, sigma: int, rho: float, time: int, seed: int
     ) -> gpd.GeoDataFrame:
-        rng_global = np.random.default_rng(787)
+        rng_global = np.random.default_rng(seed=seed)
         # Number of observations
         gdf = self.spatial_df()
         n_obs = len(gdf)
@@ -38,7 +47,7 @@ class SpatialReg(DataPull):
         X = np.ones((n_obs, 4))
 
         for i in range(1, 4):
-            rng = np.random.default_rng(seed=self.seed + 1)
+            rng = np.random.default_rng(seed=seed + i)
             X[:, i] = rng.normal(loc=mu, scale=sigma, size=n_obs)
 
         # Define Beta coefficients
@@ -50,37 +59,28 @@ class SpatialReg(DataPull):
 
         u = rng_global.normal(loc=0, scale=2, size=n_obs).reshape(-1, 1)
 
-        # Define the spatial weight matrix
-        wr = weights.contiguity.Rook.from_dataframe(gdf, use_index=False)
-        wr = wr.transform = "r"
-
-        wq = weights.contiguity.Queen.from_dataframe(gdf, use_index=False)
-        wq.transform = "r"
-
-        wk6 = weights.KNN.from_dataframe(gdf, k=6, use_index=False)
-        wk6.transform = "r"
-
         # calculate the spatial lag
-        y_d = dgp_lag(u, xb, wq, rho=rho)
-        gdf["y_d"] = y_d
+        y_true = dgp_lag(u, xb, self.wq, rho=rho)
+
+        gdf["y_true"] = y_true
 
         # pre calculate the spatial lag and apply them
         gdf["X_1"] = X[:, 1]
         gdf["X_2"] = X[:, 2]
         gdf["X_3"] = X[:, 3]
-        gdf["w_rook"] = weights.lag_spatial(wr, y_d)
-        gdf["w_queen"] = weights.lag_spatial(wq, y_d)
-        gdf["w_knn6"] = weights.lag_spatial(wk6, y_d)
+        gdf["w_rook"] = weights.lag_spatial(self.wr, y_true)
+        gdf["w_queen"] = weights.lag_spatial(self.wq, y_true)
+        gdf["w_knn6"] = weights.lag_spatial(self.wk6, y_true)
         gdf["time"] = time
 
         return gdf
 
-    def spatial_panel(self, time, rho):
+    def spatial_panel(self, time, rho, seed):
         gdf = gpd.GeoDataFrame(
-            [
+            columns=[
                 "zipcode",
                 "geometry",
-                "y_d",
+                "y_true",
                 "X_1",
                 "X_2",
                 "X_3",
@@ -92,68 +92,266 @@ class SpatialReg(DataPull):
         )
         for time_period in range(0, time):
             # Remove columns with all NA values from gdf and tmp
-            tmp = self.spatial_data(mu=2, sigma=3, rho=rho, time=time_period)
-            # gdf = gdf.dropna(axis=1, how='all')
-            # tmp = tmp.dropna(axis=1, how='all')
+            tmp = self.spatial_data(mu=2, sigma=3, rho=rho, time=time_period, seed=seed)
+            tmp = tmp.dropna(axis=1, how="all")
 
             gdf = pd.concat([gdf, tmp]).reset_index(drop=True)
 
         return gdf
 
-    def spatial_simulation(self, time, rho, simulations):
-        df = pl.DataFrame(
+    def spatial_simulation(self, time, rho, simulations, start_seed):
+        df_simulation = pl.DataFrame(
             [
-                pl.Series("bayes_x", [], dtype=pl.Float64),
-                pl.Series("bayes_w", [], dtype=pl.Float64),
-                pl.Series("freq_x", [], dtype=pl.Float64),
-                pl.Series("freq_w", [], dtype=pl.Float64),
-                pl.Series("simulations_id", [], dtype=pl.Int32),
+                pl.Series("bayes_rook_X1", [], dtype=pl.Float64),
+                pl.Series("bayes_rook_X2", [], dtype=pl.Float64),
+                pl.Series("bayes_rook_X3", [], dtype=pl.Float64),
+                pl.Series("bayes_rook_rho", [], dtype=pl.Float64),
+                pl.Series("bayes_queen_X1", [], dtype=pl.Float64),
+                pl.Series("bayes_queen_X2", [], dtype=pl.Float64),
+                pl.Series("bayes_queen_X3", [], dtype=pl.Float64),
+                pl.Series("bayes_queen_rho", [], dtype=pl.Float64),
+                pl.Series("bayes_knn6_X1", [], dtype=pl.Float64),
+                pl.Series("bayes_knn6_X2", [], dtype=pl.Float64),
+                pl.Series("bayes_knn6_X3", [], dtype=pl.Float64),
+                pl.Series("bayes_knn6_rho", [], dtype=pl.Float64),
+                pl.Series("freq_rook_X1", [], dtype=pl.Float64),
+                pl.Series("freq_rook_X2", [], dtype=pl.Float64),
+                pl.Series("freq_rook_X3", [], dtype=pl.Float64),
+                pl.Series("freq_rook_rho", [], dtype=pl.Float64),
+                pl.Series("freq_queen_X1", [], dtype=pl.Float64),
+                pl.Series("freq_queen_X2", [], dtype=pl.Float64),
+                pl.Series("freq_queen_X3", [], dtype=pl.Float64),
+                pl.Series("freq_queen_rho", [], dtype=pl.Float64),
+                pl.Series("freq_knn6_X1", [], dtype=pl.Float64),
+                pl.Series("freq_knn6_X2", [], dtype=pl.Float64),
+                pl.Series("freq_knn6_X3", [], dtype=pl.Float64),
+                pl.Series("freq_knn6_rho", [], dtype=pl.Float64),
+                pl.Series("simulknations_id", [], dtype=pl.Int32),
             ]
         )
         for i in range(0, simulations):
-            gdf = self.spatial_panel(time=time, rho=rho)
-            wr = weights.contiguity.Rook.from_dataframe(gdf[gdf["time"] == 0])
-            wr.transform = "r"
+            start_seed += 1
+            gdf = self.spatial_panel(time=time, rho=rho, seed=start_seed)
 
-            y_d = gdf["y_d"].values.reshape(-1, 1)
-            xb = gdf["X1"].values.reshape(-1, 1)
-            fe_lag = Panel_FE_Lag(y=y_d, x=xb, w=wr)
+            df = gdf.drop("geometry", axis=1)
 
-            gdf = gdf.drop("geometry", axis=1)
-            model = bmb.Model("y_d ~ 1 + X1 + w_d", gdf, dropna=True)
-            results = model.fit(cores=10)
-            az_summary = az.summary(
-                results, hdi_prob=0.95
-            )  # You can adjust hdi_prob if needed
-            means = az_summary["mean"]
+            # Bayesian Regression
+            results_rook = self.bayes_reg(data=df, weight="w_rook")
+            az_rook = az.summary(results_rook, hdi_prob=0.95)
+            bayes_rook = az_rook["mean"]
 
+            results_queen = self.bayes_reg(data=df, weight="w_queen")
+            az_queen = az.summary(results_queen, hdi_prob=0.95)
+            bayes_queen = az_queen["mean"]
+
+            results_knn6 = self.bayes_reg(data=df, weight="w_knn6")
+            az_knn6 = az.summary(results_knn6, hdi_prob=0.95)
+            bayes_knn6 = az_knn6["mean"]
+
+            # Freq Regression
+            results_rook = self.freq_reg(data=df, weights="w_rook")
+            results_queen = self.freq_reg(data=df, weights="w_queen")
+            results_knn6 = self.freq_reg(data=df, weights="w_knn6")
+
+            # Saving the Results
             df_sim = pl.DataFrame(
                 [
-                    pl.Series("bayes_x", [means.iloc[1]], dtype=pl.Float64),
-                    pl.Series("bayes_w", [means.iloc[3]], dtype=pl.Float64),
-                    pl.Series("freq_x", [fe_lag.betas[0][0]], dtype=pl.Float64),
-                    pl.Series("freq_w", [fe_lag.betas[1][0]], dtype=pl.Float64),
-                    pl.Series("simulations_id", [i], dtype=pl.Int32),
+                    pl.Series("bayes_rook_X1", [bayes_rook["X_1"]], dtype=pl.Float64),
+                    pl.Series("bayes_rook_X2", [bayes_rook["X_2"]], dtype=pl.Float64),
+                    pl.Series("bayes_rook_X3", [bayes_rook["X_3"]], dtype=pl.Float64),
+                    pl.Series("bayes_rook_rho", [bayes_rook["rho"]], dtype=pl.Float64),
+                    pl.Series("bayes_queen_X1", [bayes_queen["X_1"]], dtype=pl.Float64),
+                    pl.Series("bayes_queen_X2", [bayes_queen["X_2"]], dtype=pl.Float64),
+                    pl.Series("bayes_queen_X3", [bayes_queen["X_3"]], dtype=pl.Float64),
+                    pl.Series(
+                        "bayes_queen_rho", [bayes_queen["rho"]], dtype=pl.Float64
+                    ),
+                    pl.Series("bayes_knn6_X1", [bayes_knn6["X_1"]], dtype=pl.Float64),
+                    pl.Series("bayes_knn6_X2", [bayes_knn6["X_2"]], dtype=pl.Float64),
+                    pl.Series("bayes_knn6_X3", [bayes_knn6["X_3"]], dtype=pl.Float64),
+                    pl.Series("bayes_knn6_rho", [bayes_knn6["rho"]], dtype=pl.Float64),
+                    pl.Series(
+                        "freq_rook_X1", [results_rook.params[1]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_rook_X2", [results_rook.params[2]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_rook_X3", [results_rook.params[3]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_rook_rho", [results_rook.params[4]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_queen_X1", [results_queen.params[1]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_queen_X2", [results_queen.params[2]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_queen_X3", [results_queen.params[3]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_queen_rho", [results_queen.params[4]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_knn6_X1", [results_knn6.params[1]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_knn6_X2", [results_knn6.params[2]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_knn6_X3", [results_knn6.params[3]], dtype=pl.Float64
+                    ),
+                    pl.Series(
+                        "freq_knn6_rho", [results_knn6.params[4]], dtype=pl.Float64
+                    ),
+                    pl.Series("simulknations_id", [i], dtype=pl.Int32),
                 ]
             )
-            df = pl.concat([df, df_sim], how="vertical")
+            df_simulation = pl.concat([df_simulation, df_sim], how="vertical")
             logging.info(f"Completed Simulation #{i} succesfully")
 
-        self.results = {
-            "bayes_x": (
-                df.select((pl.col("bayes_x") - rho) ** 2).sum() / simulations
-            ).item(),
-            "bayes_w": (
-                df.select((pl.col("bayes_w") - rho) ** 2).sum() / simulations
-            ).item(),
-            "freq_x": (
-                df.select((pl.col("freq_x") - rho) ** 2).sum() / simulations
-            ).item(),
-            "freq_w": (
-                df.select((pl.col("freq_w") - rho) ** 2).sum() / simulations
-            ).item(),
-        }
-        return df
+            self.results = {
+                # Bayes Rook
+                "bayes_rook_X1": (
+                    df_simulation.select((pl.col("bayes_rook_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_rook_X2": (
+                    df_simulation.select((pl.col("bayes_rook_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_rook_X3": (
+                    df_simulation.select((pl.col("bayes_rook_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_rook_rho": (
+                    df_simulation.select((pl.col("bayes_rook_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+                # Bayes Queen
+                "bayes_queen_X1": (
+                    df_simulation.select((pl.col("bayes_queen_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_queen_X2": (
+                    df_simulation.select((pl.col("bayes_queen_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_queen_X3": (
+                    df_simulation.select((pl.col("bayes_queen_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_queen_rho": (
+                    df_simulation.select((pl.col("bayes_queen_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+                # Bayes KNN6
+                "bayes_knn6_X1": (
+                    df_simulation.select((pl.col("bayes_knn6_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_knn6_X2": (
+                    df_simulation.select((pl.col("bayes_knn6_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_knn6_X3": (
+                    df_simulation.select((pl.col("bayes_knn6_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "bayes_knn6_rho": (
+                    df_simulation.select((pl.col("bayes_knn6_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+                # Frequency Rook
+                "freq_rook_X1": (
+                    df_simulation.select((pl.col("freq_rook_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_rook_X2": (
+                    df_simulation.select((pl.col("freq_rook_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_rook_X3": (
+                    df_simulation.select((pl.col("freq_rook_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_rook_rho": (
+                    df_simulation.select((pl.col("freq_rook_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+                # Frequency Queen
+                "freq_queen_X1": (
+                    df_simulation.select((pl.col("freq_queen_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_queen_X2": (
+                    df_simulation.select((pl.col("freq_queen_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_queen_X3": (
+                    df_simulation.select((pl.col("freq_queen_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_queen_rho": (
+                    df_simulation.select((pl.col("freq_queen_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+                # Frequency KNN6
+                "freq_knn6_X1": (
+                    df_simulation.select((pl.col("freq_knn6_X1") - 5) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_knn6_X2": (
+                    df_simulation.select((pl.col("freq_knn6_X2") - 6) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_knn6_X3": (
+                    df_simulation.select((pl.col("freq_knn6_X3") - 7) ** 2).sum()
+                    / simulations
+                ).item(),
+                "freq_knn6_rho": (
+                    df_simulation.select((pl.col("freq_knn6_rho") - rho) ** 2).sum()
+                    / simulations
+                ).item(),
+            }
+
+        return df_simulation
+
+    def bayes_reg(self, data: pd.DataFrame, weight: str):
+        y_true = data["y_true"].values
+        X_1 = data["X_1"].values
+        X_2 = data["X_2"].values
+        X_3 = data["X_3"].values
+        w = data[weight].values
+
+        with pm.Model() as model:
+            # Define Priors
+            sigma = pm.HalfCauchy("sigma", beta=10)
+            intercept = pm.Normal("intercept", 0, sigma=20)
+            beta_1 = pm.Normal("X_1", 0, sigma=10)
+            beta_2 = pm.Normal("X_2", 0, sigma=10)
+            beta_3 = pm.Normal("X_3", 0, sigma=10)
+            rho = pm.Normal("rho", 0, sigma=10)
+
+            # Define likelihood
+            likelihood = pm.Normal(
+                "y_true",
+                mu=intercept + beta_1 * X_1 + beta_2 * X_2 + beta_3 * X_3 + rho * w,
+                sigma=sigma,
+                observed=y_true,
+            )
+
+            idata = pm.sample(draws=3000, chains=10, progressbar=False)
+
+        return idata
+
+    def freq_reg(self, data: pd.DataFrame, weights: str):
+        xb = data[["X_1", "X_2", "X_3", weights]].values.reshape(-1, 4)
+        y_true = data["y_true"].values.reshape(-1, 1)
+        X = sm.add_constant(xb)
+        return sm.OLS(y_true, X).fit()
 
     def spatial_df(self) -> gpd.GeoDataFrame:
         gdf = gpd.GeoDataFrame(self.make_spatial_table())
